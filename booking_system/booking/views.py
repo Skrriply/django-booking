96aaf4b4de2f6b6dcdb224e64000451f95a2f7bd
Skrriply <176331248+Skrriply@users.noreply.github.com>
2025-02-02 15:23:29 +0200

    Add type hints

--- booking_system/booking/views.py
+++ booking_system/booking/views.py
@@ -1,17 +1,20 @@
+from typing import Any
+
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from .models import Location
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     locations = Location.objects.all()  # type: ignore
     return render(request, 'index.html', {'locations': locations})
 
 
-def location_detail(request, location_id):
+def location_detail(request: HttpRequest, location_id: int) -> HttpResponse:
     location = get_object_or_404(Location, pk=location_id)
     return render(request, 'location_detail.html', {'location': location})
 
 
-def info_page(request):
+def info_page(request: HttpRequest) -> HttpResponse:
     return render(request, 'info_page.html')
