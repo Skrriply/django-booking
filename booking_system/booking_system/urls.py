0953aaec70b61a3e425af2d4b849316e8cab0962
Skrriply <176331248+Skrriply@users.noreply.github.com>
2025-02-02 15:39:37 +0200

    Organize URL configurations

--- booking_system/booking_system/urls.py
+++ booking_system/booking_system/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from booking import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('location/<int:location_id>/', views.location_detail, name='location_detail'),
-    path('information/', views.info_page, name='information about hotel page'),
+    path('', include('booking.urls')),
 ]
