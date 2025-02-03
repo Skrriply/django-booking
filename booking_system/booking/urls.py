63baf5636bf9b0d6b5de9dfe1a62424424782535
NazarUsius <nazar.gadzhalov@gmail.com>
2025-02-03 14:25:38 +0200

    Added names of urls

--- booking_system/booking/urls.py
+++ booking_system/booking/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from booking import views
+from . import views
+
+app_name = 'booking'
 
 urlpatterns = [
     path('', views.index, name='index'),
