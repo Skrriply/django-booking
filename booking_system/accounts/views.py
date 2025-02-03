dcc9a37dde8287f90ec0539e68ef4b3f43e0fa2d
NazarUsius <nazar.gadzhalov@gmail.com>
2025-02-03 14:48:11 +0200

    Added login_view

--- booking_system/accounts/views.py
+++ booking_system/accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout, get_user_model
+
+from .forms import LoginForm
 # Create your views here.
+
+
+def login_view(request):
+    if request.method == "GET":
+        form = LoginForm()
+        return render(request, 'accounts/login_page.html', {"form": form})
+    elif request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user:
+                login(request, user)
+                return redirect('booking:index')
+
+        messages.error(request, 'Something went wrong...')
+        return render(request, 'accounts/login_page.html', {"form": form})
