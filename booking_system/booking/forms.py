2a22b671eed253f31c2bfb40c68fd12ca165062e
Slekatom <nemo.bma@gmail.com>
2025-02-05 19:08:24 +0200

    Added forms for creating locations

---
+++ booking_system/booking/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+class LocationForm(forms.Form):
+    name = forms.CharField(max_length=30, min_length=3, required=True)
+    country = forms.CharField(max_length=20, required=True)
+    city = forms.CharField(max_length=20, required=True)
+    region = forms.CharField(max_length=20, required=True)
+    street = forms.CharField(max_length=30, required=True)
+    rating = forms.DecimalField(max_digits=5, decimal_places=2,
+                                 required=True)
+    amount = forms.IntegerField(required=True)
+    description = forms.CharField(max_length=100, required=True)
+    photo = forms.URLField(required=True)
+    price_per_night = forms.DecimalField(max_digits=10, 
+                                         decimal_places=2)
+
