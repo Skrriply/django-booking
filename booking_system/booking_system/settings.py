260dccca7fbc08c4b7e3857bc3c6cbae3a52fe78
NazarUsius <nazar.gadzhalov@gmail.com>
2025-02-09 15:54:54 +0200

    Installed crispy_forms and crispy_bootstrap5

--- booking_system/booking_system/settings.py
+++ booking_system/booking_system/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'booking'
+    'booking',
+
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
