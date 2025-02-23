4c75d742a535c3838359a3e8c153df9dae9b624f
Slekatom <nemo.bma@gmail.com>
2025-02-23 18:33:37 +0200

    changesss

--- booking_system/accounts/views.py
+++ booking_system/accounts/views.py
@@ -1,5 +1,5 @@
 from typing import Union
-
+import random
 from django.contrib import messages
 from django.contrib.auth import authenticate, get_user_model, login, logout
 from django.contrib.auth.decorators import login_required
@@ -60,6 +60,16 @@ def logout_view(request: HttpRequest) -> HttpResponse:
 def profile_view(request: HttpRequest) -> HttpResponse:
     user = request.user
     bookings = user.bookings.all()  # Получаем все бронирования пользователя
+    profile_image = random.choice(["https://znaj.ua/crops/73f946/360x0/1/0/2021/04/27/nFI7Mfj5D3syK7n0lz7AVAk2FFiLoGbTSHsHGSet.jpeg", 
+                                  "https://img.freepik.com/premium-photo/man-without-face-impersonal-man-mannequin-anonymous-portrait-man-abstract-identity-illustration_91497-8366.jpg",
+                                  "https://i1.sndcdn.com/artworks-fSYqOdPlOY4HvTOL-GjJEKw-t500x500.jpg",
+                                  "https://media.slovoidilo.ua/media/publications/19/183871/183871-1_large.jpg",
+                                  "https://static.wikia.nocookie.net/willcraft-animations/images/4/4f/Herobrine_111.png/revision/latest?cb=20170618083041",
+                                  "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png",
+                                  "https://mir-s3-cdn-cf.behance.net/project_modules/disp/ea7a3c32163929.567197ac70bda.png",
+                                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3xbitvBXWXb3Z86QjvGBcdvpBn5KFgrP8-g&s",
+                                  "https://cs4.pikabu.ru/post_img/big/2015/05/03/3/1430621033_2071402264.jpg",
+                                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnsT8hJ0mRUsyIf9qaIhAFlJREWrh_1nrrqw&s"])
     return render(
-        request, 'accounts/profile.html', {'user': user, 'bookings': bookings}
+        request, 'accounts/profile.html', {'user': user, 'bookings': bookings, "image": profile_image}
     )
