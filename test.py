19a95392c083da5c293afe7dd7a8e89ad0ec5ba7
Skrriply <176331248+Skrriply@users.noreply.github.com>
2025-03-09 19:29:04 +0200

    Delete ads/ and test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect("db.sqlite3")
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-print(cursor.fetchall())  # Выведет список таблиц
-conn.close()
