302b4ea1a3643884064c2125452694b6c9246776
Skrriply <176331248+Skrriply@users.noreply.github.com>
2025-03-16 19:25:54 +0200

    Rewrite accounts views

--- booking_system/accounts/views.py
+++ booking_system/accounts/views.py
@@ -3,56 +3,45 @@ from typing import Union
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.http import (
-    HttpRequest,
-    HttpResponse,
-    HttpResponsePermanentRedirect,
-    HttpResponseRedirect,
-)
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 
 from .forms import LoginForm, RegisterForm
 
 
-def login_view(
-    request: HttpRequest,
-) -> Union[HttpResponse, Union[HttpResponseRedirect, HttpResponsePermanentRedirect]]:
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+def login_view(request: HttpRequest) -> HttpResponse:
+    form = LoginForm(request.POST or None)
 
-            user = authenticate(request, username=username, password=password)
+    if request.method == 'POST' and form.is_valid():
+        user = authenticate(
+            request,
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password'],
+        )
+        if user:
+            login(request, user)
+            return redirect('booking:index')
+        messages.error(request, 'Invalid username or password.')
 
-            if user:
-                login(request, user)
-                return redirect('booking:index')
-
-        messages.error(request, 'Something went wrong...')
-        return render(request, 'accounts/login_page.html', {'form': form})
-
-    form = LoginForm()
     return render(request, 'accounts/login_page.html', {'form': form})
 
 
 def register_view(request: HttpRequest) -> HttpResponse:
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('booking:index')
-        messages.warning(request, 'Sorry, but something went wrong...')
+    form = RegisterForm(request.POST or None)
 
-    form = RegisterForm()
+    if request.method == 'POST' and form.is_valid():
+        user = form.save()
+        login(request, user)
+        return redirect('booking:index')
+
     return render(request, 'accounts/register_page.html', {'form': form})
 
 
 @login_required
 def logout_view(request: HttpRequest) -> HttpResponse:
     logout(request)
-    messages.success(request, 'You successfully loged out.')
+    messages.success(request, 'Ви успішно вийшли з облікового запису.')
+
     return redirect('booking:index')
 
 
@@ -60,6 +49,7 @@ def logout_view(request: HttpRequest) -> HttpResponse:
 def profile_view(request: HttpRequest) -> HttpResponse:
     user = request.user
     bookings = user.bookings.all()
+
     return render(
         request, 'accounts/profile.html', {'user': user, 'bookings': bookings}
     )
